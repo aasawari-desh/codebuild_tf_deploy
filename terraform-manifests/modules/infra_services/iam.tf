718c59fd3550d17723cd722900f2108d9839f872
aasawari-desh <aasawarideshmukh34@gmail.com>
2025-11-27 21:47:42 +0530

    Update iam.tf

--- terraform-manifests/modules/infra_services/iam.tf
+++ terraform-manifests/modules/infra_services/iam.tf
@@ -23,16 +23,18 @@ resource "aws_iam_policy" "ec2_policy1" {
 
 #Create a role
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role
-resource "aws_iam_policy" "ec2_policy" {
-  name        = "ec2_policy"
-  description = "Policy for EC2 access"
-  policy      = jsonencode({
+resource "aws_iam_role" "ec2_s3_access_role" {
+  name = "ec2_s3_access_role"
+
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = ["ec2:*"]
-        Resource = "*"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
       }
     ]
   })
@@ -63,6 +65,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.cloud_env}-ec2-role"
   role = aws_iam_role.ec2_s3_access_role.name
 }
+
 
 
 
