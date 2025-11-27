496e9c43b69fff91b4347cdff6b4b29b3609ed0c
aasawari-desh <aasawarideshmukh34@gmail.com>
2025-11-27 22:42:00 +0530

    Refactor IAM policies and roles in iam.tf

--- terraform-manifests/modules/infra_services/iam.tf
+++ terraform-manifests/modules/infra_services/iam.tf
@@ -1,7 +1,7 @@
 #Create a policy
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy
 
-resource "aws_iam_policy" "ec2_policy1" {
+resource "aws_iam_policy" "ec2_policy" {
   name        = "${var.cloud_env}_ec2_policy"
   path        = "/"
   description = "Policy to provide permission to EC2"
@@ -62,14 +62,14 @@ resource "aws_iam_policy" "ec2_policy" {
 
 
 # path.module is the filesystem path of the module where the expression is placed.
-resource "aws_iam_role" "ec2_s3_access_role1" {
+resource "aws_iam_role" "ec2_s3_access_role" {
   name               = "${var.cloud_env}-ec2-role"
   assume_role_policy = "${file("${path.module}/assumerolepolicy.json")}"
 }
 
 #Attach role to policy
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment
-resource "aws_iam_role_policy_attachment" "ec2_policy_role1" {
+resource "aws_iam_role_policy_attachment" "ec2_policy_role" {
   # name       = "${var.cloud_env}_ec2_attachment"
   role       = aws_iam_role.ec2_s3_access_role.name
   policy_arn = aws_iam_policy.ec2_policy.arn
@@ -85,6 +85,7 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.cloud_env}-ec2-role"
   role = aws_iam_role.ec2_s3_access_role.name
 }
+
 
 
 
